db262af43e635820723d51b809a8fab0ce3e1c23
Specter <106793765+IamSpectera@users.noreply.github.com>
2024-09-21 02:56:13 -0400

    Tiny Fix
    Login css

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
-import './login.css'; 
+import '../styles/login.css';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
